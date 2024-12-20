7f9a1ce1d53c22f24b58efeac984c0953d7aeb24
jdav892 <jadavila9@gmail.com>
2024-12-20 13:24:07 -0500

    feat: creates a an h1 within the section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,13 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      Hero
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of AI Chatting with Brainwave
+          </h1>
+        </div>
+      </div>
     </Section>
   );
 };
