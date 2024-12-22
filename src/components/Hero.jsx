2235acbab5a5364dd582584dd0ada9d4d8d30262
jdav892 <jadavila9@gmail.com>
2024-12-22 01:59:56 -0500

    fix: fixes type in tailwind config so that hero section image has gradient border

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -65,6 +65,7 @@ const Hero = () => {
                 </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
