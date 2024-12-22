94cac0aada28424c28de2dcf978afe3172337177
jdav892 <jadavila9@gmail.com>
2024-12-21 23:56:33 -0500

    feat: creates a paragraph element with a curve image for an underline on span element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import Section from "./Section";
+import curve from "../assets/hero/curve.png";
 
 const Hero = () => {
   return (
@@ -13,7 +14,21 @@ const Hero = () => {
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI Chatting with Brainwave
+            <span className="inline-block relative">
+              Brainwave
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
           </h1>
+          <p>
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the free, and open AI chat application.
+          </p>
         </div>
       </div>
     </Section>
