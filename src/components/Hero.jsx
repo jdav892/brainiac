19cb37c5a8d3ed7ab33dd464216952fdc5f64b33
jdav892 <jadavila9@gmail.com>
2024-12-23 00:17:22 -0500

    fix: fixes typo in hero section h1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of AI Chatting with{" "}
             <span className="inline-block relative">
               Brainwave
               <img
