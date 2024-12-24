42ba5fb2dd05ca3bd6d7226ed849723d2fc94631
jdav892 <jadavila9@gmail.com>
2024-12-24 15:14:50 -0500

    feat: changes cards in benefits section to have a pointer on hover

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -22,7 +22,7 @@ const Benefits = () => {
               }}
               key={item.id}
             >
-              <div className=" relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <div className=" relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] cursor-pointer">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
@@ -32,7 +32,7 @@ const Benefits = () => {
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider ">
                     Explore More
                   </p>
                   <Arrow />
