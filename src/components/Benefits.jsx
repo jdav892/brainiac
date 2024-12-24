b01501548cffe644537478faad4c29aa3ea72807
jdav892 <jadavila9@gmail.com>
2024-12-23 20:04:47 -0500

    feat: adds classNames and hover effects to cards for styling

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,6 +1,9 @@
 import Section from "./Section";
 import Heading from "./Heading";
 import { benefits } from "../constants";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -32,8 +35,27 @@ const Benefits = () => {
                   <p className="ml-auto font-code text.xs font-bold text-n-1 uppercase tracking-wider">
                     Explore More
                   </p>
+                  <Arrow />
                 </div>
               </div>
+              {item.light && <GradientLight />}
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+              <ClipPath />
             </div>
           ))}
         </div>
